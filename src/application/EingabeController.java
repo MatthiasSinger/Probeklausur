bc5167232f014b9a5c1d82b3051c9b8208fc16c1
Matthias <matze.singer@gmail.com>
2017-06-22 11:13:27 +0200

    Commit 2

--- src/application/EingabeController.java
+++ src/application/EingabeController.java
@@ -2,11 +2,30 @@ package application;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class EingabeController {
 
+	
 	@FXML Button butSpeichern;
+	@FXML Label labDurchschnitt;
+	@FXML Label labKosten;
+	@FXML Label labGesamt;
+	@FXML DatePicker datePicker1;
+	@FXML TextField tfPreis;
+	@FXML TextField tfGetankt;
+	@FXML TextField tfGefahren;
 
-	@FXML public void speichern() {}
-
+	@FXML
+	public void initialize()
+	{
+	}
+	
+	@FXML public void speichern() 
+	{
+		
+	}
+	
 }
